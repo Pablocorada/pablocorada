dd2bed0202da2009b5e62715ce9bda056c2f03e8
Pablo Corada <p.coradahernandez@gmail.com>
2022-01-20 14:22:50 +0100

    docs actualizados 2

--- webpack.prod.js
+++ webpack.prod.js
@@ -58,28 +58,23 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: './src/index.html',
             filename: './index.html',
-            chunks: ['index']
            
         }),
         new HtmlWebPackPlugin({
             template: './src/about.html',
             filename: './about.html',
-            chunks: ['about']
         }),
         new HtmlWebPackPlugin({
             template: './src/contact.html',
             filename: './contact.html',
-            chunks: ['contact']
         }),
         new HtmlWebPackPlugin({
             template: './src/projects.html',
             filename: './projects.html',
-            chunks: ['projects']
         }),
         new HtmlWebPackPlugin({
             template: './src/skills.html',
             filename: './skills.html',
-            chunks: ['skills']
         }),
         new MiniCssExtractPlugin({
             filename: '[name].[contenthash].css',
